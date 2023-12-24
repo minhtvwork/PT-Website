2f7b0c0e5374c15b0d126ae645cfd559a846f048
minh <minhtv.work@gmail.com>
2023-12-24 22:40:57 +0700

    Api

---
+++ PhuongThaoShop/PTWeb_Host/PTWeb_Api/AppDbContext/ApplicationDbContext.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PTWeb_Models.Entities;
+
+namespace PTWeb_Api.AppDbContext
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext()
+        {
+                
+        }
+        public ApplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public virtual DbSet<Ram> Rams { get; set; }
+        public virtual DbSet<Cpu> Cpus { get; set; }
+        public virtual DbSet<Color> Colors { get; set; }
+        public virtual DbSet<HardDrive> HardDrives { get; set; }
+        public virtual DbSet<Screen> Screens { get; set; }
+        public virtual DbSet<CardVGA> CardVGAs { get; set; }
+        public virtual DbSet<Bill> Bills { get; set; }
+        public virtual DbSet<BillDetail> BillDetails { get; set; }
+        public virtual DbSet<Cart> Carts { get; set; }
+        public virtual DbSet<CartDetail> CartDetails { get; set; }
+        public virtual DbSet<GiamGia> GiamGias { get; set; }
+        public virtual DbSet<Image> Images { get; set; }
+        public virtual DbSet<Serial> Serials { get; set; }
+        public virtual DbSet<LichSuTieuDiem> LichSuTieuDiems { get; set; }
+        public virtual DbSet<Manufacturer> Manufacturers { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<ProductType> ProductTypes { get; set; }
+        public virtual DbSet<ProductDetail> ProductDetails { get; set; }
+        public virtual DbSet<QuyDoiDiem> QuyDoiDiems { get; set; }
+        public virtual DbSet<Role> Roles { get; set; }
+        public virtual DbSet<SanPhamGiamGia> SanPhamGiamGias { get; set; }
+        public virtual DbSet<User> Users { get; set; }
+        public virtual DbSet<ViDiem> ViDiems { get; set; }
+        public virtual DbSet<Voucher> Vouchers { get; set; }
+        public virtual DbSet<ManagePost> ManagePosts { get; set; }
+        public virtual DbSet<Contact> Contacts { get; set; }
+    }
+}
---
+++ PhuongThaoShop/PTWeb_Host/PTWeb_Api/Controllers/AuthController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PTWeb_Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+    }
+}
