adc8f1687df4cec7d1eebcdcb73b293e88422028
minh <minhtv.work@gmail.com>
2023-12-24 22:39:49 +0700

    Entity

---
+++ PhuongThaoShop/PTWeb_Host/PTWeb_Models/Entities/Role.cs
@@ -0,0 +1,18 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PTWeb_Models.Entities
+{
+    [Table("Role")]
+    public class Role
+    {
+        [Key]
+        public Guid Id { get; set; } 
+        public string? RoleName { get; set; }
+        public int Status { get; set; }
+        public virtual ICollection<User>? Bills { get; set; }
+
+
+    }
+}
---
+++ PhuongThaoShop/PTWeb_Host/PTWeb_Models/Entities/User.cs
@@ -0,0 +1,26 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PTWeb_Models.Entities
+{
+    [Table("User")]
+    public class User 
+    {
+        [Key]
+        public Guid Id { get; set; }    
+        public string? Username { get; set; }    
+        public string? PasswordHash { get; set; }
+        public string? FullName { get; set; }
+        public string? PhoneNumber { get; set; }
+        public string? Address { get; set; }
+        public string? Email { get; set; } 
+        public Guid RoleId { get; set; }
+        public int Status { get; set; }
+        public virtual Role? Users { get; set; }
+        public virtual ViDiem? ViDiem { get; set; }
+        public virtual Cart? Cart { get; set; }
+        public virtual ICollection<Bill>? Bills { get; set; }
+
+    }
+}
