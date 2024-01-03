0259956ddf5a0a2671ff6a0ddae67b8e48760913
minh <minhtv.work@gmail.com>
2024-01-03 23:31:15 +0700

    Utility

---
+++ PhuongThaoShop/PTWeb_Host/PTWeb_Api/Utilities/QueryUtils.cs
@@ -0,0 +1,11 @@
+﻿namespace PTWeb_Api.Utilities
+{
+    public class QueryUtils
+    {
+        public static string FormatSortInput(IQueryable query,string sort)
+        {
+            
+            return sort;
+        }
+    }
+}
